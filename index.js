4a9380fb84073c56195cde86a5f645d3c1ff1f72
Guilherme <guiduck02@gmail.com>
2020-02-19 23:59:47 -0300

    set up route and added google strat to passport

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ you: 'fag' });
-});
+//taking passport and saying how it should understand googleStrat
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback' //the route user gets sent to after granting permition on google
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
+
+//when user comes to this route, passport uses strategy called google
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['pofile', 'email']
+  })
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
