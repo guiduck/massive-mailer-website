77f02ed0cc6fd9a472502204d2e110a6655f59eb
Guilherme <guiduck02@gmail.com>
2020-02-29 22:56:03 -0300

    done with the main auth flow

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const mongoose = require('mongoose');
 require('./models/User');
 require('./services/passport');
@@ -8,6 +10,16 @@ const keys = require('./config/keys');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+//these are the cookie thingys - tells passport to use cookies to handle auth
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, //time need to be in msecs
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 //the require returns the function in module.exports and we immediatly invoke the function required
 require('./routes/authRoutes')(app);
--- services/passport.js
+++ services/passport.js
@@ -6,6 +6,17 @@ const keys = require('../config/keys');
 //this is a mongoose model class and can be used to create an instance
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    //again, then cause it first need he other thing to be done
+    done(null, user);
+  });
+});
+
 //taking passport and saying how it should understand googleStrat
 passport.use(
   new GoogleStrategy(
@@ -15,8 +26,17 @@ passport.use(
       callbackURL: '/auth/google/callback' //the route user gets sent to after granting permition on google
     },
     (accessToken, refreshToken, profile, done) => {
-      //create instance of user in js and use save() to persist that on the db
-      new User({ googleId: profile.id }).save();
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          //we already have an user with that id on our db
+          done(null, existingUser);
+        } else {
+          //create instance of user in js and use save() to persist that on the db
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user)); //then cause it is async and should be done after
+        }
+      });
     }
   )
 );
