89ff6f57674085f2310d1dff0131b1bd2b03c81b
Guilherme <guiduck02@gmail.com>
2020-03-13 20:25:04 -0300

    made facebook auth. bute getting access token error

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,5 +1,7 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
+  //this will take request from browser on that relative route and send it to target + relative route
   app.use(proxy(['/api', '/auth/google'], { target: 'http://localhost:5000' }));
+  app.use(proxy('/auth/google', { target: 'http://localhost:5000' }));
 };
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+
+const FacebookStrategy = require('passport-facebook').Strategy;
+
 const mongoose = require('mongoose');
 require('./models/User');
 require('./services/passport');
@@ -10,6 +13,58 @@ const keys = require('./config/keys');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+//this is a mongoose model class and can be used to create an instance
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    //again, then cause it first need he other thing to be done
+    done(null, user);
+  });
+});
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: keys.facebookAppID,
+      clientSecret: keys.facebookAppSecret,
+      callbackURL: '/auth/facebook/callback',
+      proxy: true
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ facebookId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          //we already have an user with that id on our db
+          done(null, existingUser);
+        } else {
+          //create instance of user in js and use save() to persist that on the db
+          new User({ facebookId: profile.id })
+            .save()
+            .then(user => done(null, user)); //then cause it is async and should be done after
+        }
+      });
+    }
+  )
+);
+
+app.get(
+  '/auth/facebook',
+  passport.authenticate('facebook', {
+    scope: ['profile']
+  })
+);
+app.get(
+  '/auth/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  }
+);
 
 //these are the cookie thingys - tells passport to use cookies to handle auth
 app.use(
