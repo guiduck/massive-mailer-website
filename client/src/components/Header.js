5eece502df5792938b49a92de17f235be31e8f3a
Guilherme <guiduck02@gmail.com>
2020-04-03 22:30:40 -0300

    header is done. next is handling payments.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href='/auth/google'>Login wth google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href='/api/logout'>Logout</a>
+          </li>
+        );
+    }
+  }
+
   render() {
     return (
       <nav className='nav-wrapper'>
-        <a className='left brand-logo'>Emaily</a>
-        <ul className='right'>
-          <li>
-            <a>Login with google</a>
-          </li>
-        </ul>
+        <Link
+          to={this.props.auth ? '/surveys' : '/'}
+          className='left brand-logo'
+        >
+          Emaily
+        </Link>
+        <ul className='right'>{this.renderContent()}</ul>
       </nav>
     );
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
