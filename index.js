2bd937e22649bfc1d8d540696ad7ef5f0470caee
Guilherme <guiduck02@gmail.com>
2020-04-11 23:13:04 -0300

    finalizing charge

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express = require('express');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const keys = require('./config/keys');
+
 require('./models/User');
 require('./services/passport');
-
-const keys = require('./config/keys');
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(bodyParser.json());
 
 //these are the cookie thingys - tells passport to use cookies to handle auth
 app.use(
