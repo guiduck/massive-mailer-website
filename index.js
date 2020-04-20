c794b1dd47f6c95247dd507276abe2c07baaaade
Guilherme <guiduck02@gmail.com>
2020-04-19 22:46:36 -0300

    adding heroku build step

--- index.js
+++ index.js
@@ -28,5 +28,16 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  //express serve production assets such as main.js or main.css
+  app.use(express.static('client/build'));
+
+  //if it doesn't recognize route, serve up index.html
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
