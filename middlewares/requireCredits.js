6fa627e6b981d43e1c5e18bdbc373fc4a6ee5c93
Guilherme <guiduck02@gmail.com>
2020-04-22 22:50:33 -0300

    next is sendgrid setup

---
+++ middlewares/requireCredits.js
@@ -0,0 +1,8 @@
+module.exports = (req, res, next) => {
+  if (!req.user.credits < 1) {
+    return res.status(403).send({ error: 'Not enough credits!' });
+  }
+
+  //if everything is good it just send it to the next missleware
+  next();
+};
