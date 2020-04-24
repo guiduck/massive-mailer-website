aaedca6ade48b42783a39fa58840d6734c41e15c
Guilherme <guiduck02@gmail.com>
2020-04-23 23:44:05 -0300

    sendgrid setup

--- config/prod.js
+++ config/prod.js
@@ -8,4 +8,5 @@ module.exports = {
   facebookAppSecret: process.env.FACEBOOK_APP_SECRET,
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
   stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+  sendGridKey: process.env.SEND_GRID_KEY,
 };
