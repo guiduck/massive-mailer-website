68f25b3d67b678d5be2e9f8cd73adf763ea20ee1
Guilherme <guiduck02@gmail.com>
2020-05-02 17:43:51 -0300

    SurveyField is complete. Next is form styling

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
-export default () => {
-  return <div>SurveyField</div>;
+export default ({ input, label }) => {
+  return (
+    <div>
+      <label> {label} </label>
+      <input {...input} />
+    </div>
+  );
 };
