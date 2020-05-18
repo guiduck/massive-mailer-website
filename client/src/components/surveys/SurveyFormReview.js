3598ee0c95d85d0d725b4f042472f5f7d972fdc1
Guilherme <guiduck02@gmail.com>
2020-05-17 23:31:11 -0300

    need to dump form values

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -2,8 +2,9 @@ import React from 'react';
 import _ from 'lodash';
 import { connect } from 'react-redux';
 import formFields from './formFields';
+import * as actions from '../../actions';
 
-const SurveyFormReview = ({ onCancel, formValues }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
   const reviewFields = _.map(formFields, ({ name, label }) => {
     return (
       <div key={name}>
@@ -17,8 +18,17 @@ const SurveyFormReview = ({ onCancel, formValues }) => {
     <div>
       <h5>Please confirm your entries</h5>
       {reviewFields}
-      <button className='yellow darken-3 btn-flat' onClick={onCancel}>
+      <button
+        className='yellow darken-3 white-text btn-flat'
+        onClick={onCancel}
+      >
         Back
+      </button>
+      <button
+        className='green btn-flat right white-text'
+        onClick={() => submitSurvey(formValues)}
+      >
+        Send Survey<i className='material-icons right'>email</i>{' '}
       </button>
     </div>
   );
@@ -28,4 +38,4 @@ function mapStateToProps(state) {
   return { formValues: state.form.surveyForm.values };
 }
 
-export default connect(mapStateToProps)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(SurveyFormReview);
